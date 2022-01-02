8e49869f2902716fd73f8f79a945fea838d65bcd
rik <rikledoux@gmail.com>
2022-01-02 16:31:30 +0100

    Three to one protocol, ik heb de Alice kant geschreven. Stap 1 (depolarisatie) heb ik nog even overgeslagen.

---
+++ three_to_one/app_alice.py
@@ -0,0 +1,38 @@
+from three_to_one import three_to_one_protocol_alice
+from netqasm.sdk import EPRSocket
+from netqasm.sdk.external import NetQASMConnection, Socket
+
+def main(app_config=None):
+
+    # Create a socket for classical communication
+
+    socket = Socket("alice","bob")
+
+    # Create a EPR socket for entanglement generation
+    epr_socket = EPRSocket("bob")
+
+
+    # Initialize Alice's NetQASM connection
+    alice = NetQASMConnection(
+        app_name=app_config.app_name,
+        epr_sockets=[epr_socket]
+    )
+
+    # Create Alice's context, initialize EPR pairs inside it and call Alice's 3->1 method. Finally, print out whether or not Alice successfully created an EPR Pair with Bob.
+    with alice:
+        epr1, epr2, epr3 = epr_socket.create(number=3)[0]
+
+
+        #depolarize (step 1)
+        pass
+
+        #alice performs her gates
+        success = three_to_one_protocol_alice(epr1,epr2,epr3,alice,socket)
+
+        if success:
+            print("Alice has kept the third qubit.")
+        else:
+            print("Alice has discarded the third qubit")
+
+if __name__ == "__main__":
+    main()
