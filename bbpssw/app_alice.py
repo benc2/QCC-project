151bfcce675f0b5cb73f7b6c8f23d4d9fa9c158a
rik <rikledoux@gmail.com>
2022-01-02 21:57:45 +0100

    Templates voor Alice en Bob ingevuld

--- bbpssw/app_alice.py
+++ bbpssw/app_alice.py
@@ -5,16 +5,24 @@ from netqasm.sdk.external import NetQASMConnection, Socket
 def main(app_config=None):
 
     # Create a socket for classical communication
-    pass
+    socket = Socket("alice","bob")
 
     # Create a EPR socket for entanglement generation
-    pass
+    epr_socket = EPRSocket("bob")
 
     # Initialize Alice's NetQASM connection
-    pass
+    alice = NetQASMConnection(
+        app_name=app_config.app_name,
+        epr_sockets=[epr_socket]
+    )
 
     # Create Alice's context, initialize EPR pairs inside it and call Alice's BBPSSW method. Finally, print out whether or not Alice successfully created an EPR Pair with Bob.
-
+    with alice:
+        q1, q2 = epr_socket.create(2)
+        alice.flush()
+        print(f"Alice has created two epr pairs")
+        result = bbpssw_protocol_alice(q1, q2, alice, socket)
+        return result
 
 if __name__ == "__main__":
     main()
--- bbpssw/app_bob.py
+++ bbpssw/app_bob.py
@@ -3,18 +3,25 @@ from netqasm.sdk import EPRSocket
 from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
 
 def main(app_config=None):
-    
     # Create a socket for classical communication
-    pass
+    socket = Socket("bob", "alice")
 
     # Create a EPR socket for entanglement generation
-    pass
+    epr_socket = EPRSocket("alice")
 
     # Initialize Bob's NetQASM connection
-    pass
-
+    bob = NetQASMConnection(
+        app_name=app_config.app_name,
+        epr_sockets=[epr_socket]
+    )
     # Create Bob's context, initialize EPR pairs inside it and call Bob's BBPSSW method. Finally, print out whether or not Bob successfully created an EPR Pair with Alice.
+    with bob:
+        q1, q2 = epr_socket.recv(2)
+        bob.flush()
+        print(f"Bob has received two qubits")
 
+        result = bbpssw_protocol_bob(q1, q2, bob, socket)
+        return result
 
 if __name__ == "__main__":
     main()
