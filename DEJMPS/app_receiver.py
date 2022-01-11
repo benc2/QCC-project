1fff99ecb4919dace04e2a02bfb01f2a6090d933
Benjamin <benjamin.oudejans@gmail.com>
2022-01-11 15:58:26 +0100

    DEJMPS poging met meerdere epr pairs aan het begin maken

--- DEJMPS/app_receiver.py
+++ DEJMPS/app_receiver.py
@@ -1,7 +1,7 @@
 from netqasm.sdk import EPRSocket
 from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
 from netqasm.sdk.toolbox.sim_states import qubit_from, to_dm, get_fidelity
-
+from netqasm.sdk.classical_communication.message import StructuredMessage
 
 def main(app_config=None):
     log_config = app_config.log_config
@@ -18,31 +18,54 @@ def main(app_config=None):
         log_config=log_config,
         epr_sockets=[epr_socket]
     )
-    with receiver:
-        epr1, epr2 = epr_socket.recv(number=2)
-        print("Bob has received the EPR pairs")
+    success = False
+    N = 3
+    epr_list = epr_socket.recv(number=N+1)
+    for i in range(N):
+        print("begin of loop Bob")
+        with receiver:
+            print("Before receive")
+            print(f"Bob: success={success}")
+            # if not success:
+            #     epr1 = epr_socket.recv()[0]
+            epr1 = epr_list[0]
+            epr2 = epr_list[i+1]
+            print("After receive")
+            receiver.flush()
+            # epr2 = epr_socket.recv()[0]
+            print("Bob has received the EPR pairs")
+            # receiver.flush()
+
+
+            # print(f"Bob received outcome {sender_outcome} from Alice")
+            epr1.rot_X(3)  # in units of pi/2, 3pi/2 = -pi/2 mod 2pi
+            epr2.rot_X(3)
+            epr1.cnot(epr2)
+            print("Bob applied his gates")
+            receiver.flush()
+            receiver_outcome = epr2.measure()
+            receiver.flush()
+            print(f"Bob measured {receiver_outcome}")
+            sender_outcome = socket.recv_structured().payload
+            receiver.flush()
+            print(f"Bob received outcome {sender_outcome} from Alice")
+
+        success = sender_outcome == receiver_outcome
+        if success:
+            print(f"Success! Alice and Bob both measured {sender_outcome}")
+        else:
+            print(f"Failure! Alice measured {sender_outcome }, but Bob measured {receiver_outcome}")
+
+            # receiver.flush()
+        socket.send_structured(StructuredMessage("Success?", success))
         receiver.flush()
-
-
-        # print(f"Bob received outcome {sender_outcome} from Alice")
-        epr1.rot_X(3)  # in units of pi/2, 3pi/2 = -pi/2 mod 2pi
-        epr2.rot_X(3)
-        epr1.cnot(epr2)
-        print("Bob applied his gates")
-        receiver.flush()
-        receiver_outcome = epr2.measure()
-        receiver.flush()
-        print(f"Bob measured {receiver_outcome}")
-        sender_outcome = socket.recv_structured().payload
-        receiver.flush()
-        print(f"Bob received outcome {sender_outcome} from Alice")
-
-
-    if sender_outcome == receiver_outcome:
-        print(f"Success! Alice and Bob both measured {sender_outcome}")
-    else:
-        print(f"Failure! Alice measured {sender_outcome }, but Bob measured {receiver_outcome}")
-
+        print("End of loop Bob")
+        print("Bob tries to free")
+        # epr2.free()
+        # receiver.flush()
+        # if not success:
+        #     epr1.free()
+        # receiver.flush()
 
 if __name__ == "__main__":
     main()
--- DEJMPS/app_sender.py
+++ DEJMPS/app_sender.py
@@ -1,5 +1,6 @@
+import numpy as np
 from netqasm.sdk import Qubit, EPRSocket
-from netqasm.sdk.external import NetQASMConnection, Socket
+from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
 from netqasm.sdk.toolbox import set_qubit_state
 from netqasm.logging.output import get_new_app_logger
 from netqasm.sdk.classical_communication.message import StructuredMessage
@@ -14,7 +15,7 @@ def main(app_config=None, phi=0., theta=0.):
     # Create a EPR socket for entanglement generation
     epr_socket = EPRSocket("receiver")
 
-    print("Starting (1 iteration of) DEJMPS protocol")
+    print("Starting DEJMPS protocol")
 
     # Initialize the connection to the backend
     sender = NetQASMConnection(
@@ -22,27 +23,63 @@ def main(app_config=None, phi=0., theta=0.):
         log_config=log_config,
         epr_sockets=[epr_socket]
     )
-    sender.flush()
-    with sender:
-        # Create EPR pairs
-        epr1, epr2 = epr_socket.create(number=2)  # note: in the paper, Eve makes the pairs and they are not
-        print("Alice has created the EPR pairs")  # necessarily EPR pairs
+    N = 3
+    epr_list = epr_socket.create(number=N+1)
+    success = False
+    for i in range(N):
         sender.flush()
-        epr1.rot_X(1)  # in units of pi/2
-        epr2.rot_X(1)
-        epr1.cnot(epr2)
-        print("Alice applied her gates")
-        sender.flush()
-        sender_outcome = epr2.measure()
-        sender.flush()  # flush only for print
-        print(f"Alice measured {sender_outcome}")
+        print(f"iteration {i+1}")
+        with sender:
+            # Create EPR pairs
+            print(f"Alice: success={success}")
+            # if not success:
+            #     epr1 = epr_socket.create()[0]  # note: in the paper, Eve makes the pairs
+            # print("Alice sent first pair")
+            # sender.flush()
+            # original_dm = get_qubit_state(epr1, reduced_dm=False)
+            # epr2 = epr_socket.create()[0]
 
-    # Send the correction information
+            epr1 = epr_list[0]
+            original_dm = get_qubit_state(epr1, reduced_dm=False)
+            epr2 = epr_list[i+1]
+            print("After receive")
+            sender.flush()
+            print("Alice has created the EPR pairs")
+            sender.flush()
+            epr1.rot_X(1)  # in units of pi/2
+            epr2.rot_X(1)
+            epr1.cnot(epr2)
+            print("Alice applied her gates")
+            sender.flush()
+            sender_outcome = epr2.measure()
+            sender.flush()  # flush only for print
+            print(f"Alice measured {sender_outcome}")
+            dm = get_qubit_state(epr1, reduced_dm=False)
 
-    socket.send_structured(StructuredMessage("Corrections", sender_outcome))
-    print(f"Alice sent her outcome {sender_outcome} to Bob")
-    sender.flush()
+            # Send the correction information
 
+            socket.send_structured(StructuredMessage("Corrections", sender_outcome))
+            print(f"Alice sent her outcome {sender_outcome} to Bob")
+            sender.flush()
+
+            phi00 = np.array([1, 0, 0, 1]) / np.sqrt(2)
+            F_in = phi00.T @ original_dm @ phi00
+            F_out = phi00.T @ dm @ phi00
+            print(f"F_in: {np.real(F_in)}, F_out: {np.real(F_out)}")
+            success = socket.recv_structured().payload
+            if not success:
+                quit()
+            sender.flush()
+            print("Alice tries to free")
+            # epr2.free()
+            # sender.flush()
+            # if not success:
+            #     print("Not free")
+            #     epr1.free()
+            #     print("Free")
+            # print("flush?")
+            # sender.flush()
+            # print("flush!")
 
 if __name__ == "__main__":
     main()
